1b75b99f375912b0a78bba4ae26d44e7a09850f9
Methuka Fernando <methuka.20242180@iit.ac.lk>
2026-01-30 17:52:02 +0530

    added a react native calendar to the menu planner page

--- cookimate-frontend/frontend/app/menuPlanerPage.tsx
+++ cookimate-frontend/frontend/app/menuPlanerPage.tsx
@@ -1,34 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { Calendar } from 'react-native-calendars';
 
-export default function MenuPlannerPage() {
+const Page = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This is the Menu Planner page</Text>
-      
-      {/* Link back Home */}
-      <Link href="/" style={styles.link}>
-        Back to Home Page
-      </Link>
+      <Calendar
+        // Props go inside the opening tag
+        onDayPress={(day) => {
+          alert(day.dateString); // Matches the 'day' argument
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 50, // Added padding so it doesn't hit the status bar
     backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 22,
-    fontWeight: '600',
-    marginBottom: 20,
-  },
-  link: {
-    fontSize: 16,
-    color: 'blue',
-    textDecorationLine: 'underline',
-  },
 });
+
+export default Page;
