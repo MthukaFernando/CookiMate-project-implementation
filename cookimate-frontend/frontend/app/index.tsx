3114b25a8f22959c2b55bffb276c3e5db82fbe6e
WenukaKithnula <wkithnula@gmail.com>
2026-01-31 11:14:07 +0530

    Created the basic reusable Card component without styling

--- cookimate-frontend/frontend/app/index.tsx
+++ cookimate-frontend/frontend/app/index.tsx
@@ -1,28 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function HomePage() {
+type NavCardProps = {
+  title: string;
+  icon: string;
+  description: string;
+};
+const NavCard = ({ title, icon, description }:NavCardProps ) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This is the Home Page</Text>
-      
-      <View style={styles.navContainer}>
-        {/* Matches loginPage.tsx */}
-        <Link href="/loginPage" style={styles.link}>Go to Login</Link>
-        
-        {/* Matches app/tools/index.tsx */}
-        <Link href="/tools" style={styles.link}>Go to Tools</Link>
-        
-        {/* Matches menuPlanerPage.tsx (Note the single 'n') */}
-        <Link href="/menuPlanerPage" style={styles.link}>Go to Menu Planner</Link>
+    <Pressable style={({ pressed }) => []}>
+      <View>
+        <Text>{icon}</Text>
       </View>
-    </View>
+      <View>
+        <Text>{title}</Text>
+        <Text>{description}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+function HomePage() {
+  return (
+    <>
+      <NavCard title="search-page" icon="🔍" description={"Testing the Reusable Card component"}></NavCard>
+       <NavCard title="search-page" icon="🔍" description={"Testing the Reusable Card component 2"}></NavCard>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
-  navContainer: { gap: 15 },
-  link: { fontSize: 18, color: 'blue', textDecorationLine: 'underline' },
-});
+const styles = StyleSheet.create({});
+
+export default HomePage;
