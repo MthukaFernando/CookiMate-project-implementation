e4d7c74ec0bcf8c72b09583d9a905772d8803af9
Methuka Fernando <methuka.20242180@iit.ac.lk>
2026-01-31 14:30:54 +0530

    added the global stylesheet

---
+++ cookimate-frontend/frontend/app/globalStyleSheet.style.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 50,
+        backgroundColor: '#f2ece2',
+    }
+})
