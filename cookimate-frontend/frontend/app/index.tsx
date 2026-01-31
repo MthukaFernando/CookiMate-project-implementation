95093e745f45cdad31286e40d0ac2d376a80476a
WenukaKithnula <wkithnula@gmail.com>
2026-01-31 12:17:36 +0530

    Added the basic css to the reusable componet card

--- cookimate-frontend/frontend/app/index.tsx
+++ cookimate-frontend/frontend/app/index.tsx
@@ -7,13 +7,13 @@ type NavCardProps = {
 };
 const NavCard = ({ title, icon, description }:NavCardProps ) => {
   return (
-    <Pressable style={({ pressed }) => []}>
-      <View>
+    <Pressable style={({ pressed }) => [styles.card ,pressed && styles.cardPressed ]}>
+      <View style={styles.iconCircle} >
         <Text>{icon}</Text>
       </View>
-      <View>
-        <Text>{title}</Text>
-        <Text>{description}</Text>
+      <View style={styles.cardTextContainer}>
+        <Text style={styles.cardTitle}>{title}</Text>
+        <Text style={styles.cardDescription}>{description}</Text>
       </View>
     </Pressable>
   );
@@ -21,13 +21,50 @@ const NavCard = ({ title, icon, description }:NavCardProps ) => {
 
 function HomePage() {
   return (
-    <>
+    <View>
       <NavCard title="search-page" icon="🔍" description={"Testing the Reusable Card component"}></NavCard>
-       <NavCard title="search-page" icon="🔍" description={"Testing the Reusable Card component 2"}></NavCard>
-    </>
+      <NavCard title="community -page" icon="🔍" description={"Testing the Reusable Card component 2"}></NavCard>
+      
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+card  : {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    padding: 15,
+    
+    borderRadius: 20,
+    marginBottom: 15,
+    alignItems: 'center',
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+     
+  
+  },
+  cardPressed: {
+    transform: [{ scale: 0.98 }],
+    backgroundColor: '#f9f9f9cb',
+  },
+  iconCircle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  cardTextContainer: { flex: 1 },
+  cardTitle: { fontSize: 18, color: '#333' },
+  cardDescription: { fontSize: 14, color: '#777', marginTop: 2 },
+
+
+
+
+});
 
 export default HomePage;
