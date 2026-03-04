261d5fbea25266feed0e1a9e9ed122f24b3ee463
AshaniHansika <ashani.20242106@iit.ac.lk>
2026-03-04 23:46:53 +0530

    Update user registration to track both level types

--- cookimate-backend/controllers/userController.js
+++ cookimate-backend/controllers/userController.js
@@ -1,9 +1,8 @@
 import User from "../models/user.js";
 import Level from "../models/levels.js";
-
+import UserProgress from "../models/UserProgress.js";  // ONLY THIS LINE ADDED
 
 // create a user
-
 export const createUser = async (req, res) => {
   try {
     const { firebaseUid, username, name } = req.body;
@@ -16,6 +15,36 @@ export const createUser = async (req, res) => {
       username: username,
       firebaseUid: firebaseUid,
     });
+    
+    // START: ONLY THIS BLOCK ADDED (8 lines)
+    // Find the first simple level (Novice Chef - level 1)
+    const firstSimpleLevel = await Level.findOne({ level: 1 });
+    
+    // Create progress tracking for the user
+    const userProgress = await UserProgress.create({
+      user: newUser._id,
+      simpleLevel: firstSimpleLevel._id,
+      simpleLevelPoints: 0,
+      gamificationLevel: 1,
+      gamificationPoints: 0,
+      stats: {
+        recipesCooked: 0,
+        favoritesSaved: 0,
+        recipesShared: 0,
+        likesReceived: 0,
+        aiGenerations: 0,
+        errorsFixed: 0,
+        weeklyPlansCompleted: 0,
+        followersCount: 0,
+        currentStreak: 0,
+        longestStreak: 0,
+        dailyChallengesDone: 0,
+        photosUploaded: 0,
+        usersHelped: 0
+      }
+    });
+    // END: ONLY THIS BLOCK ADDED
+
     res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -53,7 +82,6 @@ export const getLevels = async (req, res) => {
 };
 
 // Edit user with , (username, name , profilepic)
-
 export const updateUser = async (req, res) => {
   try {
     const { username, name, profilePic } = req.body;
@@ -85,9 +113,7 @@ export const updateUser = async (req, res) => {
   }
 };
 
-
 //the api that will let the user add recips to the fav (the id of the recips will be stored in the user collection under favorites array )
-
 export const addToFavorites = async (req, res) => {
   try {
     const { recipeId } = req.body;
@@ -103,22 +129,17 @@ export const addToFavorites = async (req, res) => {
     const user = await User.findOne({ firebaseUid: uid });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
-
-
     }
     //check if the recips id is already there this is importat as we can give the user a feedbcak msg
-
     if (user.favorites.includes(recipeId)) {
       return res.status(400).json({ message: "Recipe already in favorites" });
     }
-
 
     //this will add the recipe _id (built in mongodb) end of the faverecipe array
     user.favorites.push(recipeId);
     await user.save();
     res.status(200).json({
       message: "Recipe added to favorites",
-      
     });
   } catch (error) {
     console.error(error);
