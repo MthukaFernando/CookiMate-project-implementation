eb3f68abc85c49874b5f447ef150bb1ae78d8733
WenukaKithnula <wkithnula@gmail.com>
2026-03-11 15:39:47 +0530

    Imported the Method and assigned the end point to the randome generate recpies

--- cookimate-backend/routes/recipeRoutes.js
+++ cookimate-backend/routes/recipeRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { getAllRecipes, getRecipeById, getSeasonalRecipes } from '../controllers/recipeController.js';
+import { getRandomRecipes } from "../controllers/recipeController.js";
 
 const router = express.Router();
+//To get recomandded recips in the home page
+router.get('/random', getRandomRecipes);
 
 // To get all recipes and search by filters
 router.get('/', getAllRecipes);
