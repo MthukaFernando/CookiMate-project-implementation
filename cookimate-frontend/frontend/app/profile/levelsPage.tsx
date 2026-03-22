73c925fb6032690fb763af53ce6e2cfe30aa5b8f
Methuka Fernando <methuka.20242180@iit.ac.lk>
2026-03-22 22:59:23 +0530

    changed levels page to connect to render

--- cookimate-frontend/frontend/app/profile/levelsPage.tsx
+++ cookimate-frontend/frontend/app/profile/levelsPage.tsx
@@ -18,9 +18,7 @@ import { auth } from "../../config/firebase";
 
 const { width } = Dimensions.get("window");
 
-const debuggerHost = Constants.expoConfig?.hostUri;
-const address = debuggerHost ? debuggerHost.split(":")[0] : "localhost";
-const API_URL = `http://${address}:5000`;
+const API_URL = `https://cookimate-project-implementation-m4on.onrender.com`;
 
 const statKeysMap: any = {
   cookRecipes: "recipesCooked",
