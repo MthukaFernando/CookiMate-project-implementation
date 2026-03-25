805f1ad516b3064e8de81cbb3f008583910ec94b
Methuka Fernando <methuka.20242180@iit.ac.lk>
2026-03-25 14:34:58 +0530

    added console logs to pinpoint cloudinary error

--- cookimate-backend/config/cloudinary.js
+++ cookimate-backend/config/cloudinary.js
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("--- CLOUDINARY DEBUG ---");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME ? "✅ Found" : "❌ MISSING");
+console.log("API Key:", process.env.CLOUDINARY_API_KEY ? "✅ Found" : "❌ MISSING");
 // 1. Basic Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
